0e6af75636f40478a3ef56ae68e873e889d18174
Christian Laino <christianmlaino@gmail.com>
2021-04-14 16:34:30 -0300

    fixed shortenString()

--- source/shared/helpers/stringHelper.js
+++ source/shared/helpers/stringHelper.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 export const shortenString = (str) => {
-  if (!str) return str;
+  if (!str || typeof str !== 'string') return '';
   if (str.length > 23) {
     return `${str.substr(0, 11)}...${str.substr(str.length - 9, str.length)}`;
   }
