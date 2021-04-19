a4b1dbaef9eeda3338d725518800b84b51b1f63d
Christian Laino <christianmlaino@gmail.com>
2021-04-19 15:27:14 -0300

    theme

--- source/ui/theme/index.js
+++ source/ui/theme/index.js
@@ -10,8 +10,57 @@ const theme = createMuiTheme({
       },
     },
   },
+  spacing: 12,
   typography: {
     fontFamily: 'Inter',
+    h1: {
+      fontWeight: 700,
+      fontSize: 26,
+      lineHeight: '31.2px',
+      color: '#111827',
+    },
+    h2: {
+      fontWeight: 600,
+      fontSize: 22,
+      lineHeight: '26.4px',
+      color: '#111827',
+    },
+    h3: {
+      fontWeight: 600,
+      fontSize: 20,
+      lineHeight: '24px',
+      color: '#111827',
+    },
+    h4: {
+      fontWeight: 500,
+      fontSize: 18,
+      lineHeight: '21.6px',
+      color: '#111827',
+    },
+    h5: {
+      fontWeight: 500,
+      fontSize: 16,
+      lineHeight: '19.2px',
+      color: '#111827',
+    },
+    subtitle1: {
+      fontWeight: 400,
+      fontSize: 16,
+      lineHeight: '19.2px',
+      color: '#6B7280',
+    },
+    subtitle2: {
+      fontWeight: 400,
+      fontSize: 14,
+      lineHeight: '16.8px',
+      color: '#6B7280',
+    },
+    caption: {
+      fontWeight: 400,
+      fontSize: 12,
+      lineHeight: '14.4px',
+      color: '#111827',
+    },
   },
 });
 
