973868d282fcb4d3573f769218d0293ec8dab361
Guillermo Puente <guillermo.ps09@gmail.com>
2021-04-19 18:43:50 -0400

    Removed content script

--- source/ContentScript/index.js
+++ source/ContentScript/index.js
@@ -1,13 +1,4 @@
-/* eslint-disable */
-console.log("helloworld from content script");
-
-window.addEventListener("message", (event) => {
-  console.log("running event listener Content Script");
-  console.log(event.data);
-
-  if (event.data.type === "test_req") {
-    window.postMessage({ type: "test_res", payload: "ok" }, "*");
-  }
-});
+// eslint-disable-next-line no-console
+console.log('helloworld from content script');
 
 export {};
