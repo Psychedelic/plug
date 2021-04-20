926bdbf15c2809d76537fe786d356a94669d2725
Christian Laino <christianmlaino@gmail.com>
2021-04-20 17:54:00 -0300

    translation

--- source/Popup/Views/Help/index.jsx
+++ source/Popup/Views/Help/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { Layout } from '@components';
 import { Header, LinkButton, MenuItem } from '@ui';
 import { useRouter } from '@components/Router';
@@ -7,11 +8,12 @@ import useStyles from './styles';
 
 const Help = () => {
   const classes = useStyles();
+  const { t } = useTranslation();
   const { navigator } = useRouter();
   const { helpItems } = useHelpItems();
   return (
     <Layout>
-      <Header value="Help" right={<LinkButton value="Close" onClick={() => navigator.navigate('home')} />} />
+      <Header value={t('help.title')} right={<LinkButton value={t('common.close')} onClick={() => navigator.navigate('home')} />} />
       <div className={classes.menuContainer}>
         {
           helpItems.map((item) => (
