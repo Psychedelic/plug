3e65c5ed9727881ec3d0a726b726448c0a831585
Christian Laino <christianmlaino@gmail.com>
2021-04-27 10:56:52 -0300

    swap preview component

---
+++ source/components/SwapInfo/styles.js
@@ -0,0 +1,28 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  assetInfo: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  icon: {
+    height: 41,
+    width: 41,
+    borderRadius: 26,
+    boxShadow:
+      '0px 0px 0px rgba(6, 44, 82, 0.1), 0px 1px 3px rgba(64, 66, 69, 0.12), 0px 2px 16px rgba(33, 43, 54, 0.08)',
+    marginBottom: theme.spacing(0.75),
+  },
+  asset: {
+    fontSize: 18,
+    marginBottom: theme.spacing(0.1),
+  },
+}));
