1b2c0e23e46e7d899cb6eec961892e3bc73193a3
Christian Laino <christianmlaino@gmail.com>
2021-04-29 16:11:34 -0300

    removed unused custom placeholder

--- source/ui/TextInput/index.jsx
+++ source/ui/TextInput/index.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import useStyles from './styles';
 
 const TextInput = ({
-  value, type, onChange, customPlaceholder, ...other
+  value, type, onChange, ...other
 }) => {
   const classes = useStyles();
 
@@ -18,24 +18,14 @@ const TextInput = ({
       type={type}
       onChange={onChange}
       {...other}
-    >
-      {
-        (customPlaceholder && value !== null && value !== '')
-        && <span className={classes.placholder}>{customPlaceholder}</span>
-      }
-    </InputBase>
+    />
   );
 };
 
 export default TextInput;
 
-TextInput.defaultProps = {
-  customPlaceholder: null,
-};
-
 TextInput.propTypes = {
   value: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
-  customPlaceholder: PropTypes.string,
 };
