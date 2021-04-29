e617e15b30425849ae8214092be6db5993220ac5
Christian Laino <christianmlaino@gmail.com>
2021-04-29 17:15:19 -0300

    select button component

---
+++ source/ui/SelectButton/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Typography } from '@material-ui/core';
+import clsx from 'clsx';
+import useStyles from './styles';
+
+const SelectButton = ({
+  value, startImage, endImage, onClick, selected, onEndImageClick,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <div className={clsx(classes.root, selected && classes.selected)} onClick={onClick}>
+      <img src={startImage} className={classes.iconLeft} />
+      <Typography variant="h3">{value}</Typography>
+      <img src={endImage} className={classes.iconRight} onClick={onEndImageClick} />
+    </div>
+  );
+};
+
+export default SelectButton;
+
+SelectButton.propTypes = {
+  value: PropTypes.string.isRequired,
+  startImage: PropTypes.string.isRequired,
+  endImage: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  selected: PropTypes.bool.isRequired,
+  onEndImageClick: PropTypes.func.isRequired,
+};
