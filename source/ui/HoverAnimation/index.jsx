82201ef12b4897341debd2dfe22b5727df59b56e
Tomas Rocchi <tomasrocchi@Tomass-MacBook-Pro.local>
2021-06-24 15:52:27 -0300

    fix(Swap-Action-button): Disabled button. Modified styles to be gray. Removed navigation and hover animation and added tooltip

--- source/ui/HoverAnimation/index.jsx
+++ source/ui/HoverAnimation/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import useStyles from './styles';
 
-const HoverAnimation = ({ children }) => {
+const HoverAnimation = ({ children, disabled }) => {
   const classes = useStyles();
 
   return (
-    <div className={classes.hoverAnimation}>
+    <div className={disabled ? '' : classes.hoverAnimation}>
       {children}
     </div>
   );
@@ -16,4 +16,9 @@ export default HoverAnimation;
 
 HoverAnimation.propTypes = {
   children: PropTypes.node.isRequired,
+  disabled: PropTypes.bool,
+};
+
+HoverAnimation.defaultProps = {
+  disabled: false,
 };
