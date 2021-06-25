692c5658f45661227423532aa253f102a5ac4a7a
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2021-06-25 14:11:10 -0500

    Enabled className as props

--- source/ui/Header/index.jsx
+++ source/ui/Header/index.jsx
@@ -3,11 +3,11 @@ import { Typography } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import useStyles from './styles';
 
-const Header = ({ center, left, right }) => {
+const Header = ({ center, left, right, className }) => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+    <div className={`${classes.root} ${className}`}>
       {
         left
         && <div className={classes.left}>{left}</div>
@@ -26,10 +26,12 @@ export default Header;
 Header.defaultProps = {
   left: null,
   right: null,
+  className: '',
 };
 
 Header.propTypes = {
   center: PropTypes.string.isRequired,
   left: PropTypes.node,
   right: PropTypes.node,
+  className: PropTypes.string,
 };
