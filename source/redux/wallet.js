7ca54b5c26655fb7160518f7f8f68d2c91ae105b
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2021-06-28 16:25:56 -0500

    Handled transaction reject in rejected

--- source/redux/wallet.js
+++ source/redux/wallet.js
@@ -4,25 +4,22 @@ import { CURRENCIES } from '@shared/constants/currencies';
 import keyRing from '@shared/utils/keyring';
 
 export const getTransactions = createAsyncThunk('wallet/getTransactions', async () => {
-  try {
-    const transactions = await keyRing.getTransactions();
-    const mapTransaction = (trx) => {
-      const type = Object.keys(trx.transfer)[0];
-      const amount = trx.tranfer[type]?.amount?.e8s; // The same regardless of the type
-      return {
-        type,
-        currency: CURRENCIES.get('ICP'),
-        amount,
-        date: new Date(trx?.timestamp),
-        value: amount * 40, /* TODO: Add helder's fee function / call to nns */
-        status: ACTIVITY_STATUS.DONE,
-        plug: null,
-      };
+  const transactions = await keyRing.getTransactions();
+  const mapTransaction = (trx) => {
+    const type = Object.keys(trx.transfer)[0];
+    const amount = trx.tranfer[type]?.amount?.e8s; // The same regardless of the type
+    return {
+      type,
+      currency: CURRENCIES.get('ICP'),
+      amount,
+      date: new Date(trx?.timestamp),
+      value: amount * 40, /* TODO: Add helder's fee function / call to nns */
+      status: ACTIVITY_STATUS.DONE,
+      plug: null,
     };
-    return transactions?.map?.(mapTransaction) || [];
-  } catch (e) {
-    console.log(e);
-  }
+  };
+
+  return transactions?.map?.(mapTransaction) || [];
 });
 
 /* eslint-disable no-param-reassign */
@@ -43,9 +40,14 @@ export const walletSlice = createSlice({
   },
   extraReducers: {
     [getTransactions.fulfilled]: (state, action) => {
-     state.transactions = action.payload;
+      state.transactions = action.payload;
     },
-  }
+    [getTransactions.rejected]: (state, action) => {
+      /* eslint-disable-next-line no-console */
+      console.log(action.error.message);
+      state.transactions = [];
+    },
+  },
 });
 
 export const { updateWalletDetails } = walletSlice.actions;
