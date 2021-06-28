29cc08890723f82d7375c59ef15d4d4fb40e6de6
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2021-06-28 16:04:19 -0500

    Moved getTransactions to AsyncThunk

--- source/redux/wallet.js
+++ source/redux/wallet.js
@@ -1,7 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { ACTIVITY_STATUS } from '@shared/constants/activity';
 import { CURRENCIES } from '@shared/constants/currencies';
 import keyRing from '@shared/utils/keyring';
+
+export const getTransactions = createAsyncThunk('wallet/getTransactions', async () => {
+  try {
+    const transactions = await keyRing.getTransactions();
+    const mapTransaction = (trx) => {
+      const type = Object.keys(trx.transfer)[0];
+      const amount = trx.tranfer[type]?.amount?.e8s; // The same regardless of the type
+      return {
+        type,
+        currency: CURRENCIES.get('ICP'),
+        amount,
+        date: new Date(trx?.timestamp),
+        value: amount * 40, /* TODO: Add helder's fee function / call to nns */
+        status: ACTIVITY_STATUS.DONE,
+        plug: null,
+      };
+    };
+    return transactions?.map?.(mapTransaction) || [];
+  } catch (e) {
+    console.log(e);
+  }
+});
 
 /* eslint-disable no-param-reassign */
 export const walletSlice = createSlice({
@@ -18,31 +40,14 @@ export const walletSlice = createSlice({
       state.name = name;
       state.emoji = emoji;
     },
-
-    getTransactions: async (state) => {
-      try {
-        const transactions = await keyRing.getTransactions();
-        const mapTransaction = (trx) => {
-          const type = Object.keys(trx.transfer)[0];
-          const amount = trx.tranfer[type]?.amount?.e8s; // The same regardless of the type
-          return {
-            type,
-            currency: CURRENCIES.get('ICP'),
-            amount,
-            date: new Date(trx?.timestamp),
-            value: amount * 40, /* TODO: Add helder's fee function / call to nns */
-            status: ACTIVITY_STATUS.DONE,
-            plug: null,
-          };
-        };
-        state.transactions = transactions.map(mapTransaction);
-      } catch (e) {
-        console.log(e);
-      }
-    },
   },
+  extraReducers: {
+    [getTransactions.fulfilled]: (state, action) => {
+     state.transactions = action.payload;
+    },
+  }
 });
 
-export const { updateWalletDetails, getTransactions } = walletSlice.actions;
+export const { updateWalletDetails } = walletSlice.actions;
 
 export default walletSlice.reducer;
