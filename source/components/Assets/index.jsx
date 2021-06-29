f3fe0faa449c138967e94db8f89760694822f4e2
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2021-06-29 11:04:58 -0500

    Called reducer & rendered assets

--- source/components/Assets/index.jsx
+++ source/components/Assets/index.jsx
@@ -1,37 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AssetItem, Button } from '@ui';
-import { CURRENCIES } from '@shared/constants/currencies';
 import { useRouter } from '@components/Router';
-
+import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import useStyles from './styles';
 
-const ASSETS = [
-  {
-    image: CURRENCIES.get('ICP').image,
-    name: CURRENCIES.get('ICP').name,
-    amount: 152.28,
-    value: 12183.29,
-    currency: CURRENCIES.get('ICP').value,
-  },
-  {
-    image: CURRENCIES.get('CYCLES').image,
-    name: CURRENCIES.get('CYCLES').name,
-    amount: 102.2913,
-    value: 102.30,
-    currency: CURRENCIES.get('CYCLES').value,
-  },
-];
+import { getAssets } from '../../redux/wallet';
+import useStyles from './styles';
 
 const Assets = () => {
   const classes = useStyles();
   const { navigator } = useRouter();
   const { t } = useTranslation();
 
+  const { assets } = useSelector((state) => state.wallet);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAssets());
+  }, []);
+
   return (
     <div className={classes.root}>
       {
-        ASSETS.map((asset) => (
+        assets.map((asset) => (
           <AssetItem {...asset} />
         ))
       }
