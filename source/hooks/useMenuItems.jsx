0ce5135902698aee45caf6ba7ab1f84646b4dd2b
Christian Laino <christianmlaino@gmail.com>
2021-07-02 12:48:40 -0300

    lock button on menu

--- source/hooks/useMenuItems.jsx
+++ source/hooks/useMenuItems.jsx
@@ -1,7 +1,10 @@
 import HelpIcon from '@assets/icons/help.png';
+import LockIcon from '@assets/icons/lock.png';
 import SettingsIcon from '@assets/icons/settings.png';
 import { useTranslation } from 'react-i18next';
 import { useRouter } from '@components/Router';
+import extension from 'extensionizer';
+import { HANDLER_TYPES } from '@background/Keyring';
 
 const useMenuItems = () => {
   const { navigator } = useRouter();
@@ -17,6 +20,15 @@ const useMenuItems = () => {
       image: HelpIcon,
       name: t('profile.help'),
       onClick: () => navigator.navigate('help'),
+    },
+    {
+      image: LockIcon,
+      name: t('profile.lock'),
+      onClick: (() => {
+        extension.runtime.sendMessage({ type: HANDLER_TYPES.LOCK, params: {} }, () => {
+          navigator.navigate('login');
+        });
+      }),
     },
   ];
 };
