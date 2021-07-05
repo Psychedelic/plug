556217755926870720bfdcf985f04a3182e61267
Tomas Rocchi <tomasrocchi@Tomass-MacBook-Pro.local>
2021-07-05 14:25:56 -0300

    feat(Activity): added loading to activity screen by wrapping it in a new LoadingWrapper

--- source/components/Activity/index.jsx
+++ source/components/Activity/index.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import extension from 'extensionizer';
 import { ActivityItem } from '@ui';
 import { HANDLER_TYPES } from '@background/Keyring';
 
+import LoadingWrapper from '@components/LoadingWrapper';
 import { setTransactions } from '../../redux/wallet';
 import useStyles from './styles';
 import EmptyState from './components/EmptyState';
@@ -11,6 +12,7 @@ import EmptyState from './components/EmptyState';
 const Activity = () => {
   const classes = useStyles();
   const { transactions } = useSelector((state) => state.wallet);
+  const [transactionsLoading, setLoading] = useState(true);
   const { icpPrice } = useSelector((state) => state.icp);
   const dispatch = useDispatch();
 
@@ -19,19 +21,22 @@ const Activity = () => {
       extension.runtime.sendMessage({ type: HANDLER_TYPES.GET_TRANSACTIONS, params: {} },
         (trxs) => {
           dispatch(setTransactions({ ...trxs, icpPrice }));
+          setLoading(false);
         });
     }
   }, [icpPrice]);
   return (
-    <div className={classes.root}>
-      {
-        transactions && transactions.length > 0
-          ? transactions.map((item, index) => (
-            <ActivityItem key={index.toString()} {...item} />
-          ))
-          : <EmptyState />
-      }
-    </div>
+    <LoadingWrapper loading={transactionsLoading} withInitialLoading>
+      <div className={classes.root}>
+        {
+          transactions && transactions.length > 0
+            ? transactions.map((item, index) => (
+              <ActivityItem key={index.toString()} {...item} />
+            ))
+            : <EmptyState />
+          }
+      </div>
+    </LoadingWrapper>
   );
 };
 
