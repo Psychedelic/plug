6040ebe7b6b6d3e0f9dadd73b8b1047e1e1686ae
Helder Oliveira <heldrida@gmail.com>
2021-07-09 18:15:29 +0100

    refactor: 💡 use app Provider (WalletInfo)

--- source/components/WalletInfo/wallet-info.stories.jsx
+++ source/components/WalletInfo/wallet-info.stories.jsx
@@ -1,11 +1,21 @@
 import WalletInfo from './index';
+import ProviderWrapper from '../../shared/ProviderWrapper';
+import store from '../../redux/store';
+import { theme } from '../../ui';
 
 export default {
   title: 'Components/WalletInfo',
   component: WalletInfo,
 };
 
-const Template = (args) => <WalletInfo {...args} />;
+const Template = (args) => (
+  <ProviderWrapper
+    store={store}
+    theme={theme}
+  >
+    <WalletInfo {...args} />
+  </ProviderWrapper>
+);
 
 export const Default = Template.bind({});
 Default.args = {};
