778615dde6a284ed4e565bf84c2667896d31867c
Helder Oliveira <heldrida@gmail.com>
2021-07-12 14:20:11 +0100

    refactor: 💡 use router wrapper (NavBar)

--- source/components/NavBar/navbar.stories.jsx
+++ source/components/NavBar/navbar.stories.jsx
@@ -1,11 +1,17 @@
 import NavBar from './index';
+import { RouterWrapper } from '../../../.storybook/decorators';
 
 export default {
   title: 'Components/NavBar',
   component: NavBar,
+  decorators: [
+    (Story) => (
+      <RouterWrapper story={Story} />
+    ),
+  ],
 };
 
-const Template = (args) => <NavBar {...args} />;
+const Template = () => <NavBar />;
 
 export const Default = Template.bind({});
 Default.args = {};
