0e88b2666e3c907bba7394999b0a8122c7de9449
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2021-07-12 13:24:37 -0500

    Added alignment fix to setting icon

--- source/hooks/useMenuItems.jsx
+++ source/hooks/useMenuItems.jsx
@@ -13,6 +13,7 @@ const useMenuItems = () => {
     {
       image: SettingsIcon,
       name: t('profile.settings'),
+      alignLeft: true,
       onClick: () => navigator.navigate('settings'),
     },
     {
