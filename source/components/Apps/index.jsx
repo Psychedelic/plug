825ac5ac2171aaf2bd1a30fc409d37edb6e4c0fd
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2021-07-15 13:28:43 -0500

    Refactored apps & connected with hook

--- source/components/Apps/index.jsx
+++ source/components/Apps/index.jsx
@@ -1,38 +1,23 @@
 import React, { useState } from 'react';
-import FleekImg from '@assets/icons/Fleek.svg';
-import DfinityImg from '@assets/icons/Dfinity.svg';
 import { useTranslation } from 'react-i18next';
 import Typography from '@material-ui/core/Typography';
 import { AppItem } from '@ui';
 import DeleteImg from '@assets/icons/delete.svg';
-import WinkingEmoji from '@assets/icons/wink.png';
+import ThinkingEmoji from '@assets/icons/thinking-emoji.svg';
+import { useApps } from '@hooks';
 import ActionDialog from '../ActionDialog';
 import useStyles from './styles';
 import EmptyState from './components/EmptyState';
 
-const APPS = [
-  {
-    id: 1,
-    name: 'fleek.ooo',
-    image: FleekImg,
-  },
-  {
-    id: 2,
-    name: 'dfinity.faucet',
-    image: DfinityImg,
-  },
-];
-
 const Apps = () => {
   const classes = useStyles();
   const { t } = useTranslation();
-  const [apps, setApps] = useState(APPS);
-
+  const { parsedApps: apps, removeApp } = useApps();
   const [open, setOpen] = useState(false);
   const [selectedApp, setSelectedApp] = useState(null);
 
-  const handleRemoveApp = (id) => {
-    setApps(apps.filter((app) => app.id !== id));
+  const handleRemoveApp = (app) => {
+    removeApp(app.url);
     setOpen(false);
   };
 
@@ -41,15 +26,15 @@ const Apps = () => {
     setOpen(true);
   };
 
-  return (
-    <div className={classes.emptyState}>
-      <img src={WinkingEmoji} className={classes.image} />
-      <Typography variant="h4">  {t('apps.comingSoonTitle')}</Typography>
-      <Typography variant="subtitle2">{t('apps.comingSoonSubtitle')}</Typography>
-    </div>
-  );
+  if (!apps.length) {
+    return (
+      <div className={classes.emptyState}>
+        <img src={ThinkingEmoji} className={classes.image} />
+        <Typography variant="h4">{t('apps.emptyTitle')}</Typography>
+      </div>
+    );
+  }
 
-  /* eslint-disable-next-line no-unreachable */
   return (
     <>
       <div className={classes.root}>
@@ -65,7 +50,7 @@ const Apps = () => {
                   <AppItem
                     key={index.toString()}
                     onClick={() => handleOpenDialog(app)}
-                    icon={DeleteImg}
+                    deleteIcon={DeleteImg}
                     action={t('common.delete')}
                     {...app}
                   />
@@ -84,7 +69,7 @@ const Apps = () => {
             content={<Typography>{t('apps.disconnectText')} <b>{selectedApp.name}</b>?</Typography>}
             button={t('common.disconnect')}
             buttonVariant="danger"
-            onClick={() => handleRemoveApp(selectedApp.id)}
+            onClick={() => handleRemoveApp(selectedApp)}
             onClose={() => setOpen(false)}
           />
         )
