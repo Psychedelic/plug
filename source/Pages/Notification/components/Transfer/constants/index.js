541a5b25695d1d8cb7ac3cad7406f094a370ef7e
Christian Laino <christianmlaino@gmail.com>
2021-07-19 18:55:00 -0300

    bigger screens

--- source/Pages/Notification/components/Transfer/constants/index.js
+++ source/Pages/Notification/components/Transfer/constants/index.js
@@ -1,11 +1,11 @@
 const SIZES = {
   width: 436,
-  dataHeightBig: 612,
-  dataHeightSmall: 529,
-  detailsHeightBig: 649,
-  detailHeightSmall: 514,
-  loginHeight: 572,
-  appConnectHeight: 401,
+  dataHeightBig: 622,
+  dataHeightSmall: 539,
+  detailsHeightBig: 659,
+  detailHeightSmall: 524,
+  loginHeight: 582,
+  appConnectHeight: 411,
 };
 
 export default SIZES;
