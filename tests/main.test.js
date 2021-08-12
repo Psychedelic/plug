6b91dfbdedac3bca27f8f093c8f1bdecb3ea77b9
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2021-08-12 11:38:04 -0500

    Added test for login tab creation

---
+++ tests/main.test.js
@@ -0,0 +1,38 @@
+/**
+ * @jest-environment ./tests/__setup__/test-enviroment
+ */
+import React from 'react';
+import App from '../source/Popup/app';
+import { create, act } from 'react-test-renderer';
+import extension from 'extensionizer';
+
+jest.mock('webextension-polyfill', () => ({
+  __esModule: true, // this property makes it work
+  default: global.browser,
+  namedExport: jest.fn(),
+}));
+
+jest.mock('extensionizer', () => ({
+  __esModule: true, // this property makes it work
+  default: {
+    ...global.browser,
+    runtime: {
+      ...global.browser.runtime,
+      sendMessage: jest.fn().mockImplementation((args, cb) => cb(args)),
+    }
+  },
+  namedExport: jest.fn()
+}));
+
+describe("Login flow", () => {
+  it("Renders inpage", async () => {
+    act(() => {
+      create(
+        <App />
+      )
+    });
+
+    expect(extension.runtime.sendMessage.mock.calls.length).toBe(1);
+    expect(browser.tabs.create.withArgs({url: 'options.html'}).calledOnce).toBe(true);
+  });
+});
