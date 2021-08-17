84d939de5d80a3881ea9ca19fce16ed832893dad
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2021-08-17 17:11:42 -0500

    Added ActionStep view

--- tests/main.test.js
+++ tests/main.test.js
@@ -2,9 +2,15 @@
  * @jest-environment ./tests/__setup__/test-enviroment
  */
 import React from 'react';
-import App from '../source/Popup/app';
 import { create, act } from 'react-test-renderer';
 import extension from 'extensionizer';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { theme } from '@ui';
+
+import Welcome from '../source/Options/Views/Welcome';
+import { ActionsStep } from '../source/Options/Views/Welcome/steps';
+import useSteps from '../source/Options/Views/Welcome/hooks/useSteps';
+import App from '../source/Popup/app';
 
 jest.mock('webextension-polyfill', () => ({
   __esModule: true,
@@ -25,7 +31,7 @@ jest.mock('extensionizer', () => ({
 }));
 
 describe("Login flow", () => {
-  it("Renders inpage", async () => {
+  it("Renders inpage", () => {
     act(() => {
       create(
         <App />
@@ -34,5 +40,49 @@ describe("Login flow", () => {
 
     expect(extension.runtime.sendMessage.mock.calls.length).toBe(1);
     expect(browser.tabs.create.withArgs({ url: 'options.html' }).calledOnce).toBe(true);
+  });
+
+  describe("Welcome Inpage", () => {
+    it("Renders Welcome inpage", () => {
+      let child;
+      let parent;
+
+      act(() => {
+        parent = create(
+          <App />
+        );
+
+        child = create(
+          <ThemeProvider theme={theme}>
+            <Welcome />
+          </ThemeProvider>
+        );
+      });
+
+      const welcomeType = child.root.children[0].type;
+
+      parent.root.findAllByType(welcomeType);
+    });
+
+    it("Renders ActionStep view", () => {
+      let parent;
+      let actionStep;
+
+      act(() => {
+        parent = create(
+          <App />
+        );
+
+        actionStep = create(
+          <ThemeProvider theme={theme}>
+            <ActionsStep handleChangeBranch={() => {}} />
+          </ThemeProvider>
+        );
+      });
+
+      const actionStepType = actionStep.root.children[0].type;
+
+      parent.root.findAllByType(actionStepType);
+    });
   });
 });
