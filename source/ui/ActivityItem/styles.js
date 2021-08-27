626c9256be33029fd48e368e2b500e49c665c6c7
Christian Laino <christianmlaino@gmail.com>
2021-08-27 14:41:37 -0300

    fix errors

--- source/ui/ActivityItem/styles.js
+++ source/ui/ActivityItem/styles.js
@@ -67,4 +67,9 @@ export default makeStyles((theme) => ({
   tooltip: {
     margin: '8px 0',
   },
+  amountContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+  },
 }));
