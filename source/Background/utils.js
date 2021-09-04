b781b099009ed13af8d9f30c49135a8f2fadf185
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2021-09-04 09:38:11 -0500

    Added utils for validation for burnXTC

--- source/Background/utils.js
+++ source/Background/utils.js
@@ -1,15 +1,36 @@
-import { validatePrincipalId, validateAccountId } from '@shared/utils/ids';
+import { validateCanisterId, validatePrincipalId, validateAccountId } from '@shared/utils/ids';
 
 import ERRORS from './errors';
+
+const validateAmount = (amount) => {
+  if (Number.isNaN(amount) || !Number.isInteger(amount) || amount < 0) {
+    return 'Invalid amount. The amount must be a positive integer. \n';
+  }
+
+  return false;
+};
 
 // eslint-disable-next-line
 export const validateTransferArgs = ({ to, amount }) => {
   let message = null;
+  message = validateAmount(amount);
+  /*
   if (Number.isNaN(amount) || !Number.isInteger(amount) || amount < 0) {
     message = 'Invalid amount. The amount must be a positive integer. \n';
   }
+  */
   if (!validatePrincipalId(to) && !validateAccountId(to)) {
     message = 'Invalid to address. The address must be a principal Id or an account Id';
+  }
+  return message ? ERRORS.CLIENT_ERROR(message) : null;
+};
+
+export const validateBurnArgs = ({ to, amount }) => {
+  let message = null;
+  message = validateAmount(amount);
+
+  if (!validateCanisterId(to)) {
+    message = 'Invalid to address. The address must be a canister Id';
   }
   return message ? ERRORS.CLIENT_ERROR(message) : null;
 };
