8ea1e565f1f7edd82e2da334f23590fd7d8485be
Tomás Rocchi <84542297+rocky-fleek@users.noreply.github.com>
2021-10-19 18:12:40 -0300

    Fix/error messages (#257)
    * Fix
    
    * Modified error messages, removed unwanted ones
    
    * Recovered errors, added silent errors check
    
    * Changed error for notification
    
    * Fixed transfer errors
    
    Co-authored-by: Maksym Boytsov <maksym.boytsov@gmail.com>

--- source/Background/Controller.js
+++ source/Background/Controller.js
@@ -16,7 +16,7 @@ import NotificationManager from '../lib/NotificationManager';
 import SIZES from '../Pages/Notification/components/Transfer/constants';
 import { getKeyringHandler, HANDLER_TYPES, getKeyringErrorMessage } from './Keyring';
 import { validateTransferArgs, validateBurnArgs } from './utils';
-import ERRORS from './errors';
+import ERRORS, { SILENT_ERRORS } from './errors';
 import plugProvider from '../Inpage/index';
 
 const DEFAULT_CURRENCY_MAP = {
@@ -724,7 +724,9 @@ backgroundController.exposeController(
     const { id: callId } = message.data.data;
     const { id: portId } = sender;
 
-    notificationManager.notificateError(errorMessage);
+    if (![Object.values(SILENT_ERRORS).map((e) => e.message)].includes(errorMessage)) {
+      notificationManager.notificateError(errorMessage);
+    }
 
     callback(ERRORS.CLIENT_ERROR(errorMessage), null, [{ portId, callId }]);
     callback(null, true);
