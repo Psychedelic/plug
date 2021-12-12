6ebcc3945aeba3511ed7e6697677bf8c59133be5
Christian Laino <christianmlaino@gmail.com>
2021-12-12 19:43:12 -0300

    Apps icon rounded square (#296)
    * rounded square plug
    
    * rounded borders on app connection
    
    Co-authored-by: Christian Laino <christianlaino@Christians-MacBook-Air.local>

--- source/ui/ActivityItem/styles.js
+++ source/ui/ActivityItem/styles.js
@@ -12,7 +12,7 @@ export default makeStyles((theme) => ({
     width: 41,
     boxShadow:
       '0px 0px 0px rgba(6, 44, 82, 0.1), 0px 1px 3px rgba(64, 66, 69, 0.12), 0px 2px 16px rgba(33, 43, 54, 0.08)',
-    borderRadius: 26,
+    borderRadius: 10,
   },
   leftContainer: {
     display: 'flex',
