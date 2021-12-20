b8c96798cc0dba84a280e642053705cd33a8c053
Tomás Rocchi <84542297+rocky-fleek@users.noreply.github.com>
2021-12-20 11:20:21 +0100

    Added decimal parsing to sign modals (#309)

--- source/Pages/Notification/components/Sign/components/Details/utils.js
+++ source/Pages/Notification/components/Sign/components/Details/utils.js
@@ -1,15 +1,20 @@
 import { capitalize } from '@shared/utils/string';
 import { TOKENS } from '@shared/constants/currencies';
 import { TRANSFER_METHOD_NAMES } from './constants';
+
+const CYCLE_DECIMALS = 12;
+const ICP_DECIMALS = 8;
+
 // eslint-disable-next-line max-len
 export const getAssetData = (canisterId) => Object.values(TOKENS).find((token) => token.canisterId === canisterId);
 
+// TODO: Do a request to DAB-JS to get the asset info and remove this hardcoding
 export const getAssetAmount = (request) => {
   const { methodName } = request || {};
   const amountInArgs = {
-    transfer: (args) => args?.[0]?.amount,
-    transferErc20: (args) => args?.[1],
-    send_dfx: (args) => args?.[0]?.amount?.e8s,
+    transfer: (args) => args?.[0]?.amount / (10 ** CYCLE_DECIMALS),
+    transferErc20: (args) => args?.[1] / (10 ** CYCLE_DECIMALS),
+    send_dfx: (args) => args?.[0]?.amount?.e8s / (10 ** ICP_DECIMALS),
   }[methodName];
   return amountInArgs(request?.decodedArguments);
 };
