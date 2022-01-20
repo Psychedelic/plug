78f762e36ec33b729f1eef2e2fc50ebb7f0dfe54
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2022-01-20 12:27:30 -0500

    Updates assets, nfts & activity

--- source/hooks/useMenuItems.jsx
+++ source/hooks/useMenuItems.jsx
@@ -1,13 +1,66 @@
+import { useDispatch, useSelector } from 'react-redux';
 import LockIcon from '@assets/icons/lock.png';
 import SettingsIcon from '@assets/icons/settings.png';
 import RefreshIcon from '@assets/icons/refresh.png';
 import { useTranslation } from 'react-i18next';
-import { useRouter } from '@components/Router';
+import { useRouter, TABS } from '@components/Router';
+import { useICPPrice } from '@redux/icp';
+import {
+  setAssets,
+  setAssetsLoading,
+  setTransactions,
+  setTransactionsLoading,
+  setCollections,
+  setCollectionsLoading
+} from '@redux/wallet';
 import { HANDLER_TYPES, sendMessage } from '@background/Keyring';
 
-const useMenuItems = () => {
+const useMenuItems = (toggleMenu) => {
   const { navigator } = useRouter();
   const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const icpPrice = useICPPrice();
+  const { principalId } = useSelector((state) => state.wallet);
+
+
+  const refreshWallet = () => {
+    navigator.navigate('home', TABS.TOKENS);
+    toggleMenu();
+
+    if (icpPrice) {
+      // NFTS
+      dispatch(setCollectionsLoading(true));
+      sendMessage({
+        type: HANDLER_TYPES.GET_NFTS,
+        params: {},
+      }, (nftCollections) => {
+        if (nftCollections?.length) {
+          dispatch(setCollections({ collections: nftCollections, principalId }));
+        }
+        dispatch(setCollectionsLoading(false));
+      });
+
+      // Transactions
+      dispatch(setTransactionsLoading(true));
+      sendMessage({
+        type: HANDLER_TYPES.GET_TRANSACTIONS,
+        params: {}
+      },  (trxs) => {
+        dispatch(setTransactions({ ...trxs, icpPrice }));
+        dispatch(setTransactionsLoading(false));
+      });
+
+      // Tokens
+      dispatch(setAssetsLoading(true));
+      sendMessage({
+        type: HANDLER_TYPES.GET_ASSETS,
+        params: {},
+      }, (keyringAssets) => {
+        dispatch(setAssets({ keyringAssets, icpPrice }));
+        dispatch(setAssetsLoading(false));
+      });
+    }
+  };
 
   return [
     {
@@ -20,7 +73,7 @@ const useMenuItems = () => {
       image: RefreshIcon,
       name: t('profile.refreshWallet'),
       alignLeft: true,
-      onClick: () => null,
+      onClick: refreshWallet,
     },
     {
       image: LockIcon,
