a3d0c4e486d94913e22ab78c4f9fc9a693d358e9
tomiir <rocchitomas@gmail.com>
2022-04-05 09:12:39 -0300

    Added WICP id to the protected ids (even though it was already being checked) (#414)
    Co-authored-by: rocky-fleek <tomas@fleek.co>

--- source/shared/constants/canisters.js
+++ source/shared/constants/canisters.js
@@ -2,4 +2,5 @@ export const PROTECTED_CATEGORIES = ['nft', 'asset'];
 export const DAB_CANISTER_ID = 'aipdg-waaaa-aaaah-aaq5q-cai';
 export const XTC_CANISTER_ID = 'aanaa-xaaaa-aaaah-aaeiq-cai';
 export const ICP_CANISTER_ID = 'ryjl3-tyaaa-aaaaa-aaaba-cai';
-export const ASSET_CANISTER_IDS = [XTC_CANISTER_ID, ICP_CANISTER_ID];
+export const WICP_CANISTER_ID = 'utozz-siaaa-aaaam-qaaxq-cai';
+export const ASSET_CANISTER_IDS = [XTC_CANISTER_ID, ICP_CANISTER_ID, WICP_CANISTER_ID];
