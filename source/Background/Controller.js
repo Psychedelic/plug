3f9976cb47aa31525d4b6437fa89821623688a32
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2022-04-18 20:49:14 -0500

    Added information module to controller

--- source/Background/Controller.js
+++ source/Background/Controller.js
@@ -4,16 +4,12 @@ import PlugController from '@psychedelic/plug-controller';
 import { BackgroundController } from '@fleekhq/browser-rpc';
 
 import { CONNECTION_STATUS } from '@shared/constants/connectionStatus';
-import { E8S_PER_ICP, CYCLES_PER_TC } from '@shared/constants/currencies';
-import { ICP_CANISTER_ID } from '@shared/constants/canisters';
-import { validatePrincipalId } from '@shared/utils/ids';
-import { areAllElementsIn } from '@shared/utils/array';
-import { XTC_FEE } from '@shared/constants/addresses';
 import {
   getApps,
   setApps,
   ConnectionModule,
   TransactionModule,
+  InformationModule,
   getProtectedIds,
 } from '@modules';
 
@@ -24,17 +20,7 @@ import {
   HANDLER_TYPES,
   getKeyringErrorMessage,
 } from './Keyring';
-import {
-  validateTransferArgs,
-  validateBurnArgs,
-  validateTransactions,
-} from './utils';
 import ERRORS, { SILENT_ERRORS } from './errors';
-
-const DEFAULT_CURRENCY_MAP = {
-  ICP: 0,
-  XTC: 1,
-};
 
 let keyring = {};
 
@@ -150,6 +136,7 @@ const secureController = async (callback, controller) => {
 
 let connectionModule;
 let transactionModule;
+let informationModule;
 init().then(() => {
   // Exposing module methods
   connectionModule = new ConnectionModule(backgroundController, secureController, keyring);
@@ -157,85 +144,10 @@ init().then(() => {
 
   transactionModule = new TransactionModule(backgroundController, secureController, keyring);
   transactionModule.exposeMethods();
+
+  informationModule = new InformationModule(backgroundController, secureController, keyring);
+  informationModule.exposeMethods();
 });
-
-const requestBalance = async (accountId, callback) => {
-  const getBalance = getKeyringHandler(HANDLER_TYPES.GET_BALANCE, keyring);
-  const icpBalance = await getBalance(accountId);
-  if (icpBalance.error) {
-    callback(ERRORS.SERVER_ERROR(icpBalance.error), null);
-  } else {
-    callback(null, icpBalance);
-  }
-};
-
-backgroundController.exposeController(
-  'requestBalance',
-  async (opts, metadata, accountId) => secureController(opts.callback, async () => {
-    const { callback, message, sender } = opts;
-
-    getApps(keyring.currentWalletId.toString(), (apps = {}) => {
-      const app = apps?.[metadata.url] || {};
-      if (app?.status === CONNECTION_STATUS.accepted) {
-        if (accountId && Number.isNaN(parseInt(accountId, 10))) {
-          callback(ERRORS.CLIENT_ERROR('Invalid account id'), null);
-        } else if (!keyring.isUnlocked) {
-          const url = qs.stringifyUrl({
-            url: 'notification.html',
-            query: {
-              callId: message.data.data.id,
-              portId: sender.id,
-              type: 'requestBalance',
-              argsJson: accountId,
-              metadataJson: JSON.stringify(metadata),
-            },
-          });
-
-          extension.windows.create({
-            url,
-            type: 'popup',
-            width: SIZES.width,
-            height: SIZES.loginHeight,
-          });
-        } else {
-          requestBalance(accountId, callback);
-        }
-      } else {
-        callback(ERRORS.CONNECTION_ERROR, null);
-      }
-    });
-  }),
-);
-
-backgroundController.exposeController(
-  'handleRequestBalance',
-  async (opts, url, subaccount, callId, portId) => {
-    const { callback } = opts;
-
-    getApps(keyring.currentWalletId.toString(), async (apps = {}) => {
-      const app = apps?.[url] || {};
-      callback(null, true);
-
-      if (app?.status === CONNECTION_STATUS.accepted) {
-        const getBalance = getKeyringHandler(
-          HANDLER_TYPES.GET_BALANCE,
-          keyring,
-        );
-        const icpBalance = await getBalance(subaccount);
-
-        if (icpBalance.error) {
-          callback(ERRORS.SERVER_ERROR(icpBalance.error), null, [
-            { portId, callId },
-          ]);
-        } else {
-          callback(null, icpBalance, [{ portId, callId }]);
-        }
-      } else {
-        callback(ERRORS.CONNECTION_ERROR, null, [{ portId, callId }]);
-      }
-    });
-  },
-);
 
 const signData = async (payload, callback) => {
   const parsedPayload = new Uint8Array(Object.values(payload));
@@ -327,69 +239,6 @@ backgroundController.exposeController(
       callback(ERRORS.SIGN_REJECTED, null, [{ portId, callId }]);
       callback(null, true); // Return true to close the modal
     }
-  },
-);
-
-backgroundController.exposeController('getPublicKey', async (opts) => {
-  const { callback } = opts;
-  try {
-    const publicKey = await keyring.getPublicKey();
-    callback(null, publicKey);
-  } catch (e) {
-    callback(ERRORS.SERVER_ERROR(e), null);
-  }
-});
-
-backgroundController.exposeController('getPrincipal', async (opts, pageUrl) => secureController(opts.callback, async () => {
-  const { callback, message, sender } = opts;
-
-  getApps(keyring.currentWalletId.toString(), async (apps = {}) => {
-    const app = apps?.[pageUrl] || {};
-    if (app?.status === CONNECTION_STATUS.accepted) {
-      if (!keyring.isUnlocked) {
-        const url = qs.stringifyUrl({
-          url: 'notification.html',
-          query: {
-            callId: message.data.data.id,
-            portId: sender.id,
-            type: 'principal',
-            metadataJson: JSON.stringify({ url: pageUrl }),
-          },
-        });
-
-        extension.windows.create({
-          url,
-          type: 'popup',
-          width: SIZES.width,
-          height: SIZES.loginHeight,
-        });
-      } else {
-        callback(
-          null,
-          keyring.state.wallets[keyring.currentWalletId].principal,
-        );
-      }
-    } else {
-      callback(ERRORS.CONNECTION_ERROR, null);
-    }
-  });
-}));
-
-backgroundController.exposeController(
-  'handleGetPrincipal',
-  async (opts, url, callId, portId) => {
-    const { callback } = opts;
-
-    getApps(keyring.currentWalletId.toString(), async (apps = {}) => {
-      const app = apps?.[url] || {};
-      callback(null, true);
-      if (app?.status === CONNECTION_STATUS.accepted) {
-        const { principal } = keyring?.state?.wallets?.[keyring?.currentWalletId] || {};
-        callback(null, principal?.toText(), [{ portId, callId }]);
-      } else {
-        callback(ERRORS.CONNECTION_ERROR, null, [{ portId, callId }]);
-      }
-    });
   },
 );
 
