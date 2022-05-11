d6a93e28ef0d50b255f014c52e840b4785eb8c7e
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2022-05-11 17:08:12 -0500

    Added e2e tests

--- tests/e2e/import.test.js
+++ tests/e2e/import.test.js
@@ -2,14 +2,23 @@ describe('Import/Create', () => {
   let chromeBrowser;
   let page;
 
-  const importButtonSelector = '#options-root > div > div.MuiContainer-root.MuiContainer-maxWidthMd > div > div:nth-child(2) > div > div > button';
-  const importInputSelector = '#options-root > div > div.MuiContainer-root.MuiContainer-maxWidthSm > div > div:nth-child(3) > div > div.jss36 > div > textarea';
-  const submitImportButtonSelector = '#options-root > div > div.MuiContainer-root.MuiContainer-maxWidthSm > div > div:nth-child(4) > div > button';
-  const passwordInputSelector = '#options-root > div > div.MuiContainer-root.MuiContainer-maxWidthSm > div > div:nth-child(3) > div > div.jss36 > div > input';
-  const confirmPasswordInputSelector = '#options-root > div > div.MuiContainer-root.MuiContainer-maxWidthSm > div > div:nth-child(4) > div > div.jss36 > div > input';
-  const submitPasswordButtonSelector = '#options-root > div > div.MuiContainer-root.MuiContainer-maxWidthSm > div > div:nth-child(5) > div > button';
+  const passwordErrorLabel = '#options-root > div > div.MuiContainer-root.MuiContainer-maxWidthSm > div > div.MuiGrid-root.makeStyles-passwordError-4.MuiGrid-item.MuiGrid-grid-xs-12 > p';
 
-  const seedphrase = 'genre noodle program casual volume reduce thumb body cause say rail grid';
+  const badSeedphrase = 'sadf adsf adfdfasd adfad adfafd sdfsd sdfsdf sdfds sdfd sdf sfsfs sdfadsf';
+
+  const getButtonCollection = async (page) => {
+    return await page.$$('button');
+  }
+
+  const getImportButton = async (page) => {
+    const buttonCollection = await getButtonCollection(page);
+    return buttonCollection[0];
+  };
+
+  const getCreateButton = async (page) => {
+    const buttonCollection = await getButtonCollection(page);
+    return buttonCollection[1];
+  };
 
   beforeAll(async () => {
     chromeBrowser = await setupChrome();
@@ -20,28 +29,171 @@ describe('Import/Create', () => {
     await page.goto(chromeData.optionsUrl);
   });
 
+  afterEach(async () => {
+    await page.close();
+  });
+
   afterAll(async () => {
     await chromeBrowser.close();
   });
 
-  test('', async () => {
-    await page.click(importButtonSelector);
-    await page.waitForSelector(importInputSelector);
-    await page.type(importInputSelector, seedphrase);
-    await page.click(submitImportButtonSelector);
+  describe('Import', () => {
+    beforeEach(async () => {
+      const importButton = await utils.getButtonWithIndex(page, 0);
+      await importButton.click();
+    });
 
-    await page.waitForSelector(passwordInputSelector);
-    await page.type(passwordInputSelector, 'TestPassword123');
+    test('Fails on incorrect seedphrase', async () => {
+      const importInputElement = await utils.getElement(page, 'textarea');
+      await importInputElement.type(badSeedphrase);
 
-    await page.waitForSelector(confirmPasswordInputSelector);
-    await page.type(confirmPasswordInputSelector, 'TestPassword123');
-    await page.click(submitPasswordButtonSelector);
+      const submitImport = await utils.getButtonWithIndex(page, 0);
+      await submitImport.click();
 
-    await page.screenshot({ path: 'screenshot.png' });
+      const updatedImportBtn = await utils.getButtonWithIndex(page, 0);
+      const importButtonClassName = await (await updatedImportBtn.getProperty('className')).jsonValue();
+      const importButtonClassNameArray = importButtonClassName.split(' ');
 
-    await page.goto(chromeData.popupUrl);
+      expect(importButtonClassNameArray).toEqual(expect.arrayContaining(['Mui-disabled']));
+    });
 
-    await page.screenshot({ path: 'screenshot2.png' });
-    expect(2).toBe(2);
+    test('Fails on missmatched passwords', async () => {
+      const importInputElement = await utils.getElement(page, 'textarea');
+      await importInputElement.type(secrets.seedphrase);
+
+      const submitImport = await utils.getButtonWithIndex(page, 0);
+      await submitImport.click();
+
+      await utils.waitForRender(500);
+      const [passwordInput, confirmPasswordInput] = await utils.getInputs(page);
+      await passwordInput.type('TestPassword123');
+      await confirmPasswordInput.type('MissMatchedPassword');
+
+      const submitPassword = await utils.getButtonWithIndex(page, 0);
+      await submitPassword.click();
+
+      await page.waitForSelector(passwordErrorLabel);
+      const labelErrorElement = await page.$(passwordErrorLabel)
+      const value = await page.evaluate(el => el.textContent, labelErrorElement);
+
+      expect(value).toBe('The passwords gotta match, smh!');
+    });
+
+    test('Fails on password shorter than 12 characters', async () => {
+      const importInputElement = await utils.getElement(page, 'textarea');
+      await importInputElement.type(secrets.seedphrase);
+
+      const submitImport = await utils.getButtonWithIndex(page, 0);
+      await submitImport.click();
+
+      const [passwordInput, confirmPasswordInput] = await utils.getInputs(page);
+      await passwordInput.type('123');
+      await confirmPasswordInput.type('123');
+
+      const submitPassword = await utils.getButtonWithIndex(page, 0);
+      await submitPassword.click();
+
+      await page.waitForSelector(passwordErrorLabel);
+      const labelErrorElement = await page.$(passwordErrorLabel)
+      const value = await page.evaluate(el => el.textContent, labelErrorElement);
+
+      expect(value).toBe('The minimum is 12 characters, smh!');
+    });
+
+    test('Correctly imports', async () => {
+      const importInputElement = await utils.getElement(page, 'textarea');
+      await importInputElement.type(secrets.seedphrase);
+
+      const submitImport = await utils.getButtonWithIndex(page, 0);
+      await submitImport.click();
+
+      const [passwordInput, confirmPasswordInput] = await utils.getInputs(page, true);
+      await passwordInput.type(secrets.password);
+      await confirmPasswordInput.type(secrets.password);
+
+      const submitPassword = await utils.getButtonWithIndex(page, 0);
+      await submitPassword.click();
+
+      await page.goto(chromeData.popupUrl);
+
+      const popupPasswordInput = await utils.getInputWithIndex(page, 0, true);
+      await popupPasswordInput.type(secrets.password);
+
+      const unlockPlugButton = await utils.getButtonWithIndex(page, 0);
+      await unlockPlugButton.click();
+
+      const [plugBanner] = await utils.getXPathElements(page, 'span', 'Alpha Release', true);
+      const value = await page.evaluate(el => el.textContent, plugBanner);
+
+      expect(value).toMatch(/Plug/i);
+    });
+  });
+
+  describe('Create', () => {
+    beforeEach(async () => {
+      const createButton = await utils.getButtonWithIndex(page, 1);
+      await createButton.click();
+    });
+
+    test('Fails on missmatched passwords', async () => {
+      const [passwordInput, confirmPasswordInput] = await utils.getInputs(page);
+      await passwordInput.type('TestPassword123');
+      await confirmPasswordInput.type('MissMatchedPassword');
+
+      const submitPasswordButton = await utils.getButtonWithIndex(page, 0);
+      await submitPasswordButton.click();
+
+      await page.waitForSelector(passwordErrorLabel);
+      const labelErrorElement = await page.$(passwordErrorLabel)
+      const value = await page.evaluate(el => el.textContent, labelErrorElement);
+
+      expect(value).toBe('The passwords gotta match, smh!');
+    });
+
+    test('Fails on password shorter than 12 characters', async () => {
+      const [passwordInput, confirmPasswordInput] = await utils.getInputs(page);
+      await passwordInput.type('123');
+      await confirmPasswordInput.type('123');
+
+      const submitPasswordButton = await utils.getButtonWithIndex(page, 0);
+      await submitPasswordButton.click();
+
+      await page.waitForSelector(passwordErrorLabel);
+      const labelErrorElement = await page.$(passwordErrorLabel)
+      const value = await page.evaluate(el => el.textContent, labelErrorElement);
+
+      expect(value).toBe('The minimum is 12 characters, smh!');
+    });
+
+    test('Correctly creates', async () => {
+      const [passwordInput, confirmPasswordInput] = await utils.getInputs(page);
+      await passwordInput.type(secrets.password);
+      await confirmPasswordInput.type(secrets.password);
+
+      const submitPasswordButton = await utils.getButtonWithIndex(page, 0);
+      await submitPasswordButton.click();
+
+      const [revealSeedphraseElement] = await utils.getXPathElements(page, 'span', 'Reveal Secret Recovery Phrase', true);
+      await revealSeedphraseElement.click();
+
+      const confirmSeedphraseElement = await utils.getInputWithIndex(page, 0);
+      await confirmSeedphraseElement.click();
+
+      const continueBtn = await utils.getButtonWithIndex(page, 0);
+      await continueBtn.click();
+
+      await page.goto(chromeData.popupUrl);
+
+      const popupPasswordInput = await utils.getInputWithIndex(page, 0, true);
+      await popupPasswordInput.type(secrets.password);
+
+      const unlockPlugButton = await utils.getButtonWithIndex(page, 0);
+      await unlockPlugButton.click();
+
+      const [plugBanner] = await utils.getXPathElements(page, 'span', 'Alpha Release', true);
+      const value = await page.evaluate(el => el.textContent, plugBanner);
+
+      expect(value).toMatch(/Plug/i);
+    });
   });
 });
