04c420a5c145a0ebbbc97ce5bb4edf6a28700191
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2022-05-20 11:16:08 -0500

    Removed useContacts hook

--- source/hooks/index.js
+++ source/hooks/index.js
@@ -1,4 +1,3 @@
-export { default as useContacts } from './useContacts';
 export { default as useHelpItems } from './useHelpItems';
 export { default as useMenuItems } from './useMenuItems';
 export { default as useSettingsItems } from './useSettingsItems';
