27e18c96def0271a38d4ebc22e04368119d5eeab
Cancu <juancruz.cancu@gmail.com>
2022-07-24 16:05:14 +0200

    fixed

--- source/components/IDInput/index.jsx
+++ source/components/IDInput/index.jsx
@@ -1,5 +1,4 @@
-
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import MInputBase from '@material-ui/core/InputBase';
