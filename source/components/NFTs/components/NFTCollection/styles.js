e72e09c1cdd49e86fa1e20107723fc1e69fcd717
rocky-fleek <tomas@fleek.co>
2022-08-05 16:27:16 +0200

    Removed extra margin

--- source/components/NFTs/components/NFTCollection/styles.js
+++ source/components/NFTs/components/NFTCollection/styles.js
@@ -2,7 +2,6 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
   collectionHeader: {
-    marginBottom: 12,
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
