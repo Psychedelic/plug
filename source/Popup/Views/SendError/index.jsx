8f5ea700e5172e040da5f4f38926a1889fa2f5e9
Cancu <juancruz.cancu@gmail.com>
2022-08-16 17:38:00 -0300

    removed string and put into translation

--- source/Popup/Views/SendError/index.jsx
+++ source/Popup/Views/SendError/index.jsx
@@ -33,7 +33,7 @@ const SendErrorScreen = () => {
         <img src={ERROR_EMOJI[errorCode] || NoMouthFace} className={styles.image} />
         <Typography variant="h4" className={styles.text}>{t(`sendError.${errorCode}.title`) || t('sendError.default.title') }</Typography>
         <Typography variant="subtitle2" className={styles.text}>{t(`sendError.${errorCode}.description`) || t('sendError.default.title')}</Typography>
-        <Typography variant="subtitle2" className={styles.errorCode}>{`Error code: #${errorCode}`}</Typography>
+        <Typography variant="subtitle2" className={styles.errorCode}>{t(`sendError.errorCode ${errorCode}`)}</Typography>
         <Button
           variant="rainbow"
           value={t('error.returnHome')}
