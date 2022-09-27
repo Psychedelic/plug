5369964ab9a6c5bfc11fe1f01cf9d88c65a26dc9
rocky-fleek <tomas@fleek.co>
2022-09-27 15:23:51 -0600

    Refactored nft collections view to add virtualized support for huge number of nfts in a collection. This is achieved by building rows of 3 elements and virtualizing such rows. Refactored nft details screen to dynamically fetch the details of an NFT on render

--- source/views/Extension/Views/NFTDetails/index.jsx
+++ source/views/Extension/Views/NFTDetails/index.jsx
@@ -1,31 +1,42 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { HttpAgent } from '@dfinity/agent';
 import {
   Layout,
   Header, Button, Badge, LinkButton, NFTDisplayer, ICNSDisplay,
 } from '@components';
 import { useTranslation } from 'react-i18next';
+import { getNFTActor } from '@psychedelic/dab-js';
+import { Typography } from '@material-ui/core';
+import { ArrowUpRight } from 'react-feather';
+import extension from 'extensionizer';
+
 import BackIcon from '@assets/icons/back.svg';
 import ExpandIcon from '@assets/icons/expand.svg';
-import { useDispatch, useSelector } from 'react-redux';
 import { TABS, useRouter } from '@components/Router';
 import CollectionImg from '@assets/icons/nfts/collection.png';
 import DescriptionImg from '@assets/icons/nfts/description.png';
 import AttributesImg from '@assets/icons/nfts/attributes.png';
 import AboutImg from '@assets/icons/nfts/about.png';
-import { Typography } from '@material-ui/core';
 import { setSelectedNft } from '@redux/nfts';
-import { ArrowUpRight } from 'react-feather';
-import extension from 'extensionizer';
 import { NFT_COLLECTION_DEFAULT_TYPES } from '@shared/constants/nft';
 
 import Section from './components/section';
 import useStyles from './styles';
+
+const getTokenDetails = async ({ index, canister, standard }) => {
+  const agent = new HttpAgent({ host: 'https://ic0.app/' });
+  const NFTActor = getNFTActor({ canisterId: canister, standard, agent });
+  const details = await NFTActor.details(index);
+  return details;
+}
 
 const NFTDetails = () => {
   const { t } = useTranslation();
   const { selectedNft: nft } = useSelector((state) => state.nfts);
   const { collections } = useSelector((state) => state.wallet);
   const { navigator: routerNavigator } = useRouter();
+  const [populatedNFT, setPopulatedNFT] = useState(nft);
   const classes = useStyles();
   const dispatch = useDispatch();
   if (!nft) {
@@ -38,15 +49,15 @@ const NFTDetails = () => {
     routerNavigator.navigate('home', TABS.NFTS);
   };
 
-  const collection = useMemo(() => collections?.find((col) => col.name === nft?.collection),
-    [collections, nft]);
+  const collection = useMemo(() => collections?.find((col) => col.canisterId === populatedNFT?.canister),
+    [collections, populatedNFT]);
 
-  const name = `${nft?.name ?? `#${nft?.index}`}`;
-  const isICNS = nft?.collection === 'ICNS';
+  const name = `${populatedNFT?.name ?? `#${populatedNFT?.index}`}`;
+  const isICNS = collection === 'ICNS';
 
   const openNFT = (url) => {
     const parsedUrl = isICNS
-      ? `https://icns.id/domains/${nft?.name.replace('.icp', '')}/detail`
+      ? `https://icns.id/domains/${populatedNFT?.name.replace('.icp', '')}/detail`
       : url;
 
     extension.tabs.create({
@@ -59,10 +70,12 @@ const NFTDetails = () => {
   // eslint-disable-next-line no-confusing-arrow
   const handleButtonClick = (url) => isICNS ? openNFT(url) : copyNFT(url);
 
-  const nftDefaultTag = NFT_COLLECTION_DEFAULT_TYPES[nft.canisterId];
+  const nftDefaultTag = NFT_COLLECTION_DEFAULT_TYPES[populatedNFT.canister];
 
   useEffect(() => {
     window.scrollTo(0, 0);
+    console.log(nft);
+    getTokenDetails(nft).then((details) => setPopulatedNFT({ ...nft, ...details }));
   }, []);
 
   return (
@@ -76,15 +89,14 @@ const NFTDetails = () => {
             src={ExpandIcon}
             data-testid="expand-nft"
             onClick={() => openNFT(nft?.url?.replace('type=thumbnail', ''))}
-            data-testid="expand-nft"
           />
         )}
       />
       <div className={classes.container}>
         {isICNS ? (
-          <ICNSDisplay icns={nft} className={classes.image} large />
+          <ICNSDisplay icns={populatedNFT} className={classes.image} large />
         ) : (
-          <NFTDisplayer url={nft?.url} className={classes.image} defaultTag={nftDefaultTag} interactive />
+          <NFTDisplayer url={populatedNFT?.url} className={classes.image} defaultTag={nftDefaultTag} interactive />
         )}
         <div className={classes.buttonContainer}>
           <Button
@@ -92,7 +104,7 @@ const NFTDetails = () => {
             value={t(`nfts.${isICNS ? 'manage' : 'copyLink'}`)}
             style={{ width: '96%' }}
             fullWidth
-            onClick={() => handleButtonClick(nft?.url?.replace('type=thumbnail', ''))}
+            onClick={() => handleButtonClick(populatedNFT?.url?.replace('type=thumbnail', ''))}
             buttonTestId="copy-link-button"
             endIcon={isICNS && (
               <ArrowUpRight
@@ -112,18 +124,18 @@ const NFTDetails = () => {
           />
         </div>
         <Section icon={CollectionImg} title={t('nfts.collection')}>
-          <Badge value={nft?.collection} icon={collection?.icon} iconClassName={classes.icnsIcon} />
+          <Badge value={collection?.name} icon={collection?.icon} iconClassName={classes.icnsIcon} />
           <Badge value={name} />
         </Section>
-        {!!nft?.desc && (
+        {!!populatedNFT?.desc && (
           <Section icon={DescriptionImg} title={t('nfts.description')}>
-            <Typography variant="subtitle1">{nft?.desc}</Typography>
+            <Typography variant="subtitle1">{populatedNFT?.desc}</Typography>
           </Section>
         )}
-        {nft?.metadata?.properties?.filter((prop) => typeof prop?.value !== 'object')?.length >= 1 && (
+        {populatedNFT?.metadata?.properties?.filter((prop) => typeof prop?.value !== 'object')?.length >= 1 && (
           <Section icon={AttributesImg} title={t('nfts.attributes')}>
             {
-              nft?.metadata?.properties?.map((prop) => ((
+              populatedNFT?.metadata?.properties?.map((prop) => ((
                 <Badge
                   name={prop.name}
                   value={prop?.value}
