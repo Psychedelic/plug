56a6cda3ae5bb531c5ce395760cc5a940a794fa0
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2022-10-06 15:39:22 -0500

    Removed update providerConnection

--- source/components/ConnectAccountsModal/index.jsx
+++ source/components/ConnectAccountsModal/index.jsx
@@ -59,21 +59,9 @@ const ConnectAccountsModal = ({
     });
   };
 
-  const updateProviderConnection = async (walletId) => {
-    const currentWallet = wallets.find(w => w.walletId === walletId) || null;
-    if (currentWallet) {
-      extension.tabs.query({ active: true }, (activeTabs) => {
-        extension.tabs.sendMessage(activeTabs[0].id, { action: 'updateConnection' });
-      });
-    }
-  };
-
   const handleConfirm = () => {
     Object.keys(walletsToUpdate).forEach((walletId) => {
-      if (walletsToUpdate[walletId]) {
-        connectAccountToTab(walletId, tab)
-        updateProviderConnection(walletId);
-      }
+      walletsToUpdate[walletId] && connectAccountToTab(walletId, tab);
     });
     onConfirm?.();
     setWalletsToUpdate({});
