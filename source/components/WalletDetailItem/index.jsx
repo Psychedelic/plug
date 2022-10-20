64ae4e6933cbbabac6d95f70f05feb3928e14545
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2022-10-20 15:11:31 -0500

    Added InfoModal & WalletDetailItem components

---
+++ source/components/WalletDetailItem/index.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Typography } from '@material-ui/core';
+import { Info } from 'react-feather';
+import { useTranslation } from 'react-i18next';
+import clsx from 'clsx';
+
+import CopyButton from '../CopyButton';
+import FormItem from '../FormItem';
+import InfoModal from '../InfoModal';
+
+import useStyles from './styles';
+
+const WalletDetailItem = ({
+  value, name, setInfoOpen, isOpen, copyButtonTestId, infoIconButtonTestId, className
+}) => {
+  const classes = useStyles();
+  const { t } = useTranslation();
+  return (
+    <div className={clsx(className?.length && className)}>
+      <FormItem
+        label={t(`common.${name}`)}
+        smallLabel
+        endIcon={<CopyButton color="black" label="Copy" text={value} placement="left" data-testid={copyButtonTestId} />}
+        component={(
+          <div className={classes.ids}>
+            <Typography variant="subtitle2" className={classes.id}>{value}</Typography>
+            <Info
+              className={classes.idInfoIcon}
+              size={20}
+              onClick={() => setInfoOpen(true)}
+              data-testid={infoIconButtonTestId}
+            />
+          </div>
+        )}
+      />
+      <InfoModal
+        title={t(`walletDetails.${name}InfoTitle`)}
+        onClose={() => setInfoOpen(false)}
+        isOpen={isOpen}
+        content={t(`walletDetails.${name}Description`)}
+        buttonText={t(`walletDetails.${name}LearnMore`)}
+        understandButtonTestId={`understand-${name}-button`}
+        learnMoreButtonTestId="learn-more-button"
+      />
+    </div>
+  );
+};
+
+WalletDetailItem.propTypes = {
+  value: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  setInfoOpen: PropTypes.func.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  copyButtonTestId: PropTypes.string,
+  infoIconButtonTestId: PropTypes.string,
+  className: PropTypes.string,
+};
+
+WalletDetailItem.defaultProps = {
+  copyButtonTestId: '',
+  infoIconButtonTestId: '',
+  className: '',
+};
+
+export default WalletDetailItem;
