ee6f1e79fb0cc37ae61510081617dc3606f1d2f3
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2022-10-24 12:38:21 -0500

    Added validate pemfile logic

--- source/views/Extension/Views/ImportWallet/hooks/useSteps.jsx
+++ source/views/Extension/Views/ImportWallet/hooks/useSteps.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { HANDLER_TYPES, sendMessage } from "@background/Keyring";
+import { toast } from 'react-toastify';
 import { LinkButton } from '@components';
 import { useTranslation } from 'react-i18next';
 import { useRouter } from '@components/Router';
@@ -12,6 +14,8 @@ const useSteps = () => {
   const { t } = useTranslation();
 
   const [userPemFile, setUserPemFile] = useState(null);
+  const [validateLoading, setValidateLoading] = useState(false);
+  const [importDisabled, setImportDisabled] = useState(true);
 
   const handleChangeStep = (index) => setStep(index);
   const handleClose = () => navigator.navigate('home');
@@ -19,11 +23,42 @@ const useSteps = () => {
   const leftButton = (onClick) => <LinkButton value={t('common.back')} onClick={onClick} startIcon={BackIcon} />;
   const rightButton = <LinkButton value={t('common.close')} onClick={handleClose} />;
 
+  useEffect(() => {
+    if (userPemFile === null) {
+      setImportDisabled(true);
+      return;
+    }
 
+    setValidateLoading(true);
+    let fileReader = new FileReader();
+    fileReader.readAsText(userPemFile);
+    fileReader.onloadend = async () => {
+      const content = fileReader.result;
+
+      sendMessage({
+        type: HANDLER_TYPES.VALIDATE_PEM,
+        params: { pem: content },
+      }, (validPem) => {
+        if (!validPem) {
+          toast('Invalid PEM file', { type: 'error' });
+        }
+        setValidateLoading(false);
+        setImportDisabled(!validPem);
+      });
+    };
+  }, [userPemFile]);
 
   const steps = [
     {
-      component: <Step1 handleChangeStep={handleChangeStep} setUserPemFile={setUserPemFile} userPemFile={userPemFile} />,
+      component: (
+        <Step1
+          handleChangeStep={handleChangeStep}
+          setUserPemFile={setUserPemFile}
+          validateLoading={validateLoading}
+          importDisabled={importDisabled}
+          userPemFile={userPemFile}
+        />
+      ),
       left: leftButton(() => handleClose()),
       right: rightButton,
       center: `${t("importPem.importPEMfile")}`,
