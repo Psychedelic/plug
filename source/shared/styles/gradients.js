edb7e536687211fda604f011f460dc90f3dd51dc
rocky-fleek <tomas@fleek.co>
2022-11-01 11:27:15 -0600

    Removed unused gradient

--- source/shared/styles/gradients.js
+++ source/shared/styles/gradients.js
@@ -1,4 +1,3 @@
 const RAINBOW_GRADIENT = 'linear-gradient(white, white), radial-gradient(circle at top left,#FFE701,#FA51D3,#10D9ED,#53FF54)';
-export const RAINBOW_GRADIENT_2 = 'linear-gradient(transparent, white), radial-gradient(circle at top left,#FFE701,#FA51D3,#10D9ED,#53FF54)';
 
 export default RAINBOW_GRADIENT;
