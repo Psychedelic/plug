2395de765faa669335c18f32a7b4aab8962dfbd0
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2022-11-01 11:20:15 -0500

    Added autofocus & cursor at hide button

--- source/views/Options/Views/Welcome/steps/ImportMnemonicStep/index.jsx
+++ source/views/Options/Views/Welcome/steps/ImportMnemonicStep/index.jsx
@@ -1,46 +1,55 @@
-import React, { useCallback, useState } from 'react';
-import PropTypes from 'prop-types';
-import { useTranslation } from 'react-i18next';
-import { Typography } from '@material-ui/core';
-import * as bip39 from 'bip39';
+import React, { useCallback, useState } from "react";
+import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
+import { Typography } from "@material-ui/core";
+import * as bip39 from "bip39";
 
-import { HANDLER_TYPES, sendMessage } from '@background/Keyring';
-import { clearStorage } from '@modules/storageManager';
-import { getRandomEmoji } from '@shared/constants/emojis';
-import { Button, FormInput } from '@components';
-import useStyles from './styles';
-import ShowHideToggle from '../../components/ShowHideToggle';
-import TextInput from '../../../../../../components/TextInput';
+import { HANDLER_TYPES, sendMessage } from "@background/Keyring";
+import { clearStorage } from "@modules/storageManager";
+import { getRandomEmoji } from "@shared/constants/emojis";
+import { Button, FormInput } from "@components";
+import useStyles from "./styles";
+import ShowHideToggle from "../../components/ShowHideToggle";
+import TextInput from "../../../../../../components/TextInput";
 
 const MNEMONIC_LENGTH = 12;
 
 export function clearClipboard() {
-  window.navigator.clipboard.writeText('');
+  window.navigator.clipboard.writeText("");
 }
 
 const isValidMnemonic = (mnemonic) => bip39.validateMnemonic(mnemonic);
-export const parseMnemonic = (mnemonic) => (mnemonic || '').trim().toLowerCase().match(/\w+/gu)?.join(' ') || '';
+export const parseMnemonic = (mnemonic) =>
+  (mnemonic || "").trim().toLowerCase().match(/\w+/gu)?.join(" ") || "";
 
 const createArray = (length, fillValue) => new Array(length).fill(fillValue);
 
 const ImportWalletStep = ({ handleNextStep, handleSetMnemonic }) => {
   const { t } = useTranslation();
   const classes = useStyles();
-  const [mnemonicError, setMnemonicError] = useState('');
+  const [mnemonicError, setMnemonicError] = useState("");
   const [pasteFailed, setPasteFailed] = useState(false);
-  const [draftMnemonic, setDraftMnemonic] = useState(createArray(MNEMONIC_LENGTH, ''));
-  const [showMnemonic, setShowMnemonic] = useState(createArray(MNEMONIC_LENGTH, false));
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [confirmPasswordError, setConfirmPasswordError] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordError, setPasswordError] = useState('');
+  const [draftMnemonic, setDraftMnemonic] = useState(
+    createArray(MNEMONIC_LENGTH, "")
+  );
+  const [showMnemonic, setShowMnemonic] = useState(
+    createArray(MNEMONIC_LENGTH, false)
+  );
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
-  const [invalidMnemonic, setInvalidMnemonic] = useState(false);
+  const [invalidMnemonic, setInvalidMnemonic] = useState(true);
 
   const handleCreateAccount = async () => {
     // clean the storage before initiating keyring
     clearStorage();
-    const params = { password, mnemonic: draftMnemonic.join(' '), icon: getRandomEmoji() };
+    const params = {
+      password,
+      mnemonic: draftMnemonic.join(" "),
+      icon: getRandomEmoji(),
+    };
     sendMessage({ type: HANDLER_TYPES.IMPORT, params }, (response) => {
       handleSetMnemonic(response?.mnemonic);
     });
@@ -49,21 +58,24 @@ const ImportWalletStep = ({ handleNextStep, handleSetMnemonic }) => {
 
   const onMnemonicChange = useCallback(
     (newDraftMnemonic) => {
-      let newMnemonicError = '';
-      const joinedDraftMnemonic = newDraftMnemonic.join(' ').trim().toLowerCase();
-      if (newDraftMnemonic.some((word) => word !== '')) {
-        if (newDraftMnemonic.some((word) => word === '')) {
-          newMnemonicError = t('seedPhraseReq');
+      let newMnemonicError = "";
+      const joinedDraftMnemonic = newDraftMnemonic
+        .join(" ")
+        .trim()
+        .toLowerCase();
+      if (newDraftMnemonic.some((word) => word !== "")) {
+        if (newDraftMnemonic.some((word) => word === "")) {
+          newMnemonicError = t("seedPhraseReq");
         } else if (!isValidMnemonic(joinedDraftMnemonic)) {
           setInvalidMnemonic(true);
-          newMnemonicError = t('invalidSeedPhrase');
+          newMnemonicError = t("invalidSeedPhrase");
         }
       }
 
       setDraftMnemonic(newDraftMnemonic);
       setMnemonicError(newMnemonicError);
     },
-    [setDraftMnemonic, setMnemonicError, t],
+    [setDraftMnemonic, setMnemonicError, t]
   );
 
   const toggleShowMnemonic = useCallback((index) => {
@@ -86,13 +98,13 @@ const ImportWalletStep = ({ handleNextStep, handleSetMnemonic }) => {
       newMnemonic[index] = newWord.trim();
       onMnemonicChange(newMnemonic);
     },
-    [draftMnemonic, onMnemonicChange],
+    [draftMnemonic, onMnemonicChange]
   );
 
   const onMnemonicPaste = useCallback(
     (rawMnemonic) => {
       const parsedMnemonic = parseMnemonic(rawMnemonic);
-      let newDraftMnemonic = parsedMnemonic.split(' ');
+      let newDraftMnemonic = parsedMnemonic.split(" ");
       if (newDraftMnemonic.length > MNEMONIC_LENGTH) {
         setPasteFailed(true);
         return;
@@ -102,50 +114,50 @@ const ImportWalletStep = ({ handleNextStep, handleSetMnemonic }) => {
       // If paste content is shorter than 12 words, fill the rest with empty strings
       if (newDraftMnemonic.length < MNEMONIC_LENGTH) {
         newDraftMnemonic = newDraftMnemonic.concat(
-          createArray(MNEMONIC_LENGTH - newDraftMnemonic.length, ''),
+          createArray(MNEMONIC_LENGTH - newDraftMnemonic.length, "")
         );
       }
       setShowMnemonic(createArray(MNEMONIC_LENGTH, false));
       onMnemonicChange(newDraftMnemonic);
       clearClipboard();
     },
-    [onMnemonicChange, pasteFailed, setPasteFailed],
+    [onMnemonicChange, pasteFailed, setPasteFailed]
   );
 
   const onPasswordChange = useCallback(
     (event) => {
       const newPassword = event.target.value;
-      let newConfirmPasswordError = '';
-      let newPasswordError = '';
+      let newConfirmPasswordError = "";
+      let newPasswordError = "";
 
       if (newPassword && newPassword.length < 8) {
-        newPasswordError = t('passwordNotLongEnough');
+        newPasswordError = t("passwordNotLongEnough");
       }
 
       if (confirmPassword && newPassword !== confirmPassword) {
-        newConfirmPasswordError = t('passwordsDontMatch');
+        newConfirmPasswordError = t("passwordsDontMatch");
       }
 
       setPassword(newPassword);
       setPasswordError(newPasswordError);
       setConfirmPasswordError(newConfirmPasswordError);
     },
-    [confirmPassword, t],
+    [confirmPassword, t]
   );
 
   const onConfirmPasswordChange = useCallback(
     (event) => {
       const newConfirmPassword = event.target.value;
-      let newConfirmPasswordError = '';
+      let newConfirmPasswordError = "";
 
       if (password !== newConfirmPassword) {
-        newConfirmPasswordError = t('passwordsDontMatch');
+        newConfirmPasswordError = t("passwordsDontMatch");
       }
 
       setConfirmPassword(newConfirmPassword);
       setConfirmPasswordError(newConfirmPasswordError);
     },
-    [password, t],
+    [password, t]
   );
 
   return (
@@ -155,32 +167,37 @@ const ImportWalletStep = ({ handleNextStep, handleSetMnemonic }) => {
           const id = `mnemonic-word-${index}`;
           return (
             <div key={index} className={classes.mnemonicWordInputContainer}>
-              <Typography className={classes.mnemonicWordLabel}>{`${index + 1}.`}</Typography>
-              <TextInput
-                id={id}
-                className={classes.mnemonicWordInput}
-                data-testid={`seedphrase-input-${index + 1}`}
-                type={showMnemonic[index] ? 'text' : 'password'}
-                onChange={(e) => {
-                  e.preventDefault();
-                  onMnemonicWordChange(index, e.target.value);
-                }}
-                value={draftMnemonic[index]}
-                autoComplete="off"
-                onPaste={(event) => {
-                  const newMnemonic = event.clipboardData.getData('text');
-                  if (newMnemonic.trim().match(/\s/u)) {
-                    event.preventDefault();
-                    onMnemonicPaste(newMnemonic);
-                  }
-                }}
-              />
-              <ShowHideToggle
-                name={`${id}-checkbox`}
-                show={showMnemonic[index]}
-                onChange={() => toggleShowMnemonic(index)}
-                label={t('MnemonicToggleShow')}
-              />
+              <Typography className={classes.mnemonicWordLabel}>
+                {`${index + 1}.`}
+              </Typography>
+              <div className={classes.mnemonicWordAction}>
+                <TextInput
+                  id={id}
+                  className={classes.mnemonicWordInput}
+                  data-testid={`seedphrase-input-${index + 1}`}
+                  type={showMnemonic[index] ? "text" : "password"}
+                  onChange={(e) => {
+                    e.preventDefault();
+                    onMnemonicWordChange(index, e.target.value);
+                  }}
+                  autoFocus={index === 0}
+                  value={draftMnemonic[index]}
+                  autoComplete="off"
+                  onPaste={(event) => {
+                    const newMnemonic = event.clipboardData.getData("text");
+                    if (newMnemonic.trim().match(/\s/u)) {
+                      event.preventDefault();
+                      onMnemonicPaste(newMnemonic);
+                    }
+                  }}
+                />
+                <ShowHideToggle
+                  name={`${id}-checkbox`}
+                  show={showMnemonic[index]}
+                  onChange={() => toggleShowMnemonic(index)}
+                  label={t("MnemonicToggleShow")}
+                />
+              </div>
             </div>
           );
         })}
@@ -190,30 +207,28 @@ const ImportWalletStep = ({ handleNextStep, handleSetMnemonic }) => {
         <div className={classes.passwordInputContainer}>
           <FormInput
             containerClassName={classes.passwordInput}
-            label={t('welcome.passwordLabel')}
+            label={t("welcome.passwordLabel")}
             value={password}
             onChange={onPasswordChange}
             type="password"
             id="password"
             error={passwordError}
-            autoFocus
             data-testid="enter-password-input"
           />
           <FormInput
             containerClassName={classes.passwordInput}
-            label={t('welcome.passwordConfirmLabel')}
+            label={t("welcome.passwordConfirmLabel")}
             value={confirmPassword}
             onChange={onConfirmPasswordChange}
             type="password"
             id="password"
             error={confirmPasswordError}
-            autoFocus
             data-testid="enter-password-input"
           />
         </div>
         <Button
           variant="rainbow"
-          value={t('welcome.importButton')}
+          value={t("welcome.importButton")}
           onClick={handleCreateAccount}
           fullWidth
           disabled={invalidMnemonic}
--- source/views/Options/Views/Welcome/steps/ImportMnemonicStep/styles.js
+++ source/views/Options/Views/Welcome/steps/ImportMnemonicStep/styles.js
@@ -1,44 +1,51 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
   mnemonicContainer: {
-    width: '100%',
+    width: "100%",
     maxWidth: 702,
   },
   mnemonicWordsContainer: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
     marginBottom: theme.spacing(2),
   },
   mnemonicWordInputContainer: {
-    width: '100%',
+    width: "100%",
     maxWidth: 200,
     margin: 15,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  mnemonicWordAction: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    gap: "8px",
   },
   mnemonicWordInput: {
     height: 30,
     maxWidth: 150,
   },
   divider: {
-    width: '100%',
+    width: "100%",
     height: 1,
-    backgroundColor: 'rgba(0, 0, 0, 0.16)',
+    backgroundColor: "rgba(0, 0, 0, 0.16)",
     marginBottom: theme.spacing(2),
   },
   footerContainer: {
-    padding: '0 45px',
+    padding: "0 45px",
   },
   passwordInputContainer: {
     marginBottom: theme.spacing(2),
-    display: 'flex',
-    justifyContent: 'space-between',
-    width: '100%',
+    display: "flex",
+    justifyContent: "space-between",
+    width: "100%",
   },
   passwordInput: {
     maxWidth: 290,
