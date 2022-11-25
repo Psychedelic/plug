8e82e78f586d1b1746c2f04aa209312a863369bc
rocky-fleek <tomas@fleek.co>
2022-11-24 21:25:57 -0600

    Added transaction status guard

--- source/Modules/storageManager.js
+++ source/Modules/storageManager.js
@@ -194,9 +194,14 @@ export const checkPendingTransaction = (transactionId, cb) => {
 };
 
 export const reviewPendingTransaction = async (transactionId) => {
+  const getTxStatus = () => new Promise((resolve) => {
+    checkPendingTransaction(transactionId, resolve);
+  });
   const setData = () => new Promise((resolve) => {
     secureSetWrapper({ activeTransactions: { [transactionId]: 'reviewed' } }, {}, resolve);
   });
+  const txStatus = await getTxStatus();
+  if (txStatus !== 'pending') throw new Error('Unauthorized review');
   const result = await setData();
   return result;
 };
